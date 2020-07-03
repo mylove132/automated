1a9df6dd3d41e52b0721d408312e42b1c8d18072
刘占会 <liuzhanhui@liuzhanhuideMacBook-Pro.local>
2020-07-03 14:30:10 +0800

    excel

---
+++ automated-common/src/main/java/com/xdf/bling/qa/common/Singleton.java
@@ -0,0 +1,19 @@
+package com.xdf.bling.qa.common;
+
+public class Singleton {
+
+    public volatile static Singleton singleton = null;
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null){
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
